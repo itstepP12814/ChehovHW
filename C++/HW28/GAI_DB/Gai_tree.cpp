1b96db3bb1d4e18523c245df8efdbdc866bfce5f
Pavel Chehov <good-1991@mail.ru>
2015-01-27 12:35:58 +0300

    refactoring 28_2 GAI_DB

--- C++/HW28/GAI_DB/Gai_tree.cpp
+++ C++/HW28/GAI_DB/Gai_tree.cpp
@@ -26,57 +26,45 @@ Gai_tree::Offence& Gai_tree::operator[](const string& index){
 	++size_of_tree;
 	return (*current)->ticket;
 }
-void Gai_tree::showTree(Car* node){
-	if (node != NULL){
-		showTree(node->leftCar);
-		print(node);
-		showTree(node->rightCar);
+void Gai_tree::showTree(Car* current_car){
+	/*Мы идем по дереву и спускаемся все время вниз(или вверх) влево, и в итоге мы натыкаемся на пустую ноду,
+	после которой условие if не выполняется и мы "поднимаемся" на одну ноду вверх, возвращаясь назад в предыдущую запущенную функцию.
+	Из-за того что проходя по дереву мы натыкаемсяна пустую NULL ноду, нужно обязательно делать печать данных машины строго ПОСЛЕ проверки условия
+	пустая нода или нет, иначе мы столкнемся с ошибкой сегментирования т.к попытаемся прочитать данные из области памяти с NULL*/
+	if (current_car != NULL){
+		showTree(current_car->leftCar);
+		print(current_car);
+		showTree(current_car->rightCar);
 	}
 }
-Gai_tree::Car* Gai_tree::search(const string& index, Car* node){
+Gai_tree::Car* Gai_tree::search(const string& index, Car* current_car){
 	//тут и далее в качестве первоначальной ноды всегда передается рутовя нода
-	if (node != NULL){
-		search(index, node->leftCar);
-		if (index == node->number){
+	if (current_car != NULL){
+		search(index, current_car->leftCar);
+		if (index == current_car->number){
 			cout << "car is find!" << endl;
-			print(node);
-			return node;
+			print(current_car);
+			return current_car;
 		}
-		search(index, node->rightCar);
+		search(index, current_car->rightCar);
 	}
 }
-void Gai_tree::searchDiapasone(const string& index1, const string& index2, Car* node){
-	//преобразование строк к числам
-	const char* temp_index1 = index1.c_str();//первый номер диапазона
-	const char* temp_index2 = index2.c_str();//второй номер диапазона
-	int ind1 = atoi(temp_index1);
-	int ind2 = atoi(temp_index2);
-
-
-	if (node != NULL){
-		/*Мы идем по дереву и спускаемся все время вниз(или вверх) влево, и в итоге мы натыкаемся на пустую ноду,
-		после которой условие первого if не выполняется и мы "поднимаемся" на одну ноду вверх, возвращаясь назад в предыдущую запущенную функцию.
-		Запускается проверка второго if на соответствие условиям поиска в диапазоне. Из-за того что проходя по дереву мы натыкаемся
-		на пустую NULL ноду, нужно обязательно делать получение номера машины (temp_node=node->number.c_str();) и преобразование к числу,
-		строго ПОСЛЕ проверки на то пустая нода или нет, если вынести преобразование номера текущей ноды до проверки первого if(например после temp_index1 и temp_index2),
-		то мы столкнемся с ошибкой сегментирования т.к попытаемся прочитать данные из области памяти с NULL*/
-
-		const char* temp_node = node->number.c_str();//номер текущей ноды(узла или машины)
-		int index_node = atoi(temp_node);
-		searchDiapasone(index1, index2, node->leftCar);
-		if (index_node >= ind1 && index_node <= ind2){
-			print(node);
+void Gai_tree::searchDiapasone(const string& index1, const string& index2, Car* current_car){
+	if (current_car != NULL){
+		searchDiapasone(index1, index2, current_car->leftCar);
+		if (index1 <= current_car->number && current_car->number <= index2){
+			print(current_car);
 		}
-		searchDiapasone(index1, index2, node->rightCar);
+		searchDiapasone(index1, index2, current_car->rightCar);
 	}
 }
 
 Gai_tree::Car* getRoot(const Gai_tree A){
 	return A.root;
 }
-void print(Gai_tree::Car* node){
-	cout << "number " << node->number << endl;
-	cout << "speeding " << node->ticket.speeding << endl;
-	cout << "illegal parkind " << node->ticket.illegal_parking << endl;
-	cout << "running on red " << node->ticket.running_on_red << endl << endl;
+void print(Gai_tree::Car* current_car){
+	cout << "number " << current_car->number << endl;
+	cout << "speeding " << current_car->ticket.speeding << endl;
+	cout << "illegal parkind " << current_car->ticket.illegal_parking << endl;
+	cout << "running on red " << current_car->ticket.running_on_red << endl << endl;
 }
--- C++/HW28/GAI_DB/main.cpp
+++ C++/HW28/GAI_DB/main.cpp
@@ -5,12 +5,12 @@ int main() {
 	Gai_tree A;
 	A["1230"] = { 8, 9, 6 };
 	A["1231"] = { 9, 7, 6 };
-	A["1232"] = { 457, 247, 817 };
-	A["1233"] = { 47, 21, 87 };
-	A["1234"] = { 23, 65, 1 };
-	A["1235"] = { 54, 23, 65 };
+	A["C123PF"] = { 457, 247, 817 };
+	A["A123AA"] = { 47, 21, 87 };
+	A["C123BG"] = { 23, 65, 1 };
+	A["C123CC"] = { 54, 23, 65 };
 	A.showTree(getRoot(A));
 	A.search("1235", getRoot(A));
-	A.searchDiapasone("1231", "1234", getRoot(A));
+	A.searchDiapasone("A123AA", "C123FF", getRoot(A));
 	return 0;
 }
