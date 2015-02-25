090ac54abd69073b9174fd3d7e2f9d40a035914a
Pavel Chehov <good-1991@mail.ru>
2015-02-25 11:38:30 +0300

    работа продолжается

--- C++/exam/Game/main.cpp
+++ C++/exam/Game/main.cpp
@@ -28,7 +28,7 @@ public:
 		vector<vector<Cell>> f(size.first, vector<Cell>(size.second, copy));
 		field = f;
 		beg.first = field.begin();
-		beg.second = (*beg.first).begin();
+		beg.second = (*field.begin()).begin();
 	};
 	~Field(){};
 	friend Player;
@@ -42,22 +42,10 @@ public:
 	Player(){};
 	Player(pair<vector<vector<Cell>>::iterator, vector<Cell>::iterator> pos) : currentPosition(pos), prevPosition(pos){};
 	~Player(){};
-	void mark(Cell& currentCell){//метод делает метку
-		if (currentCell.wasHere == false){
-			currentCell.wasHere = true;
-		}
-		else {
-			cout << "end!" << endl;
-		}
-	}
 	void mark(pair<vector<vector<Cell>>::iterator, vector<Cell>::iterator> pos, Field &field){
-		ptrdiff_t dist_row = distance(vector<vector<Cell>>::iterator(currentPosition.first), vector<vector<Cell>>::iterator(field.field.end()));
-		ptrdiff_t dist_col = distance(vector<Cell>::iterator((*(field.field.begin() + dist_row)).begin()), vector<Cell>::iterator((*(field.field.begin() + dist_row)).end()));
-		dist_col = distance( vector<Cell>::iterator ((*field.field.begin()).begin()), vector<Cell>::iterator ((*field.field.end()).end()));
-		/*if (field.field[dist_row][dist_col].wasHere == false){
-			field.field[dist_row][dist_col].wasHere == true;
-		}*/
-
+		if ((*currentPosition.second).wasHere == false){
+			(*currentPosition.second).wasHere = true;
+		}
 	}
 
 	void switcher(int t, Field &field){
@@ -77,18 +65,16 @@ public:
 		}
 	}
 	void goRigth(Field &field){
-		//посчитали разницу между строками текущей позиции и концом поля
-		ptrdiff_t dist_row = distance(vector<vector<Cell>>::iterator(currentPosition.first), vector<vector<Cell>>::iterator(field.field.end()));
 
 		if (currentPosition.first != prevPosition.first){//если мы пришли с другого ряда, то поворачиваем соотвественно в пределах текущего ряда
 			if (currentPosition.first > prevPosition.first){//если номер предыдущего ряда меньше, значит мы шли снизу вверх, вправо сдвинемься на одну колонку
 				prevPosition = currentPosition;//делаем текущую позицию предыдущей, после чего высчитываем новую текущую позицию
-				if (currentPosition.second != (*(field.field.begin() + dist_row)).end())//проверяем не уперлись ли мы в конец ряда
+				if (currentPosition.second != (*field.field.begin()).end())//проверяем не уперлись ли мы в конец ряда
 					currentPosition.second = ++currentPosition.second;//идем право
 			}
 			else {//иначе - шли серху вниз, сдвинемься на одну колонку
 				prevPosition = currentPosition;
-				if (currentPosition.second != (*(field.field.begin() + dist_row)).begin())
+				if (currentPosition.second != (*field.field.begin()).begin())
 					currentPosition.second = --currentPosition.second;//идем лево
 			}
 		}
@@ -100,8 +86,9 @@ public:
 			}
 			else{
 				prevPosition = currentPosition;
-				if (currentPosition.first != field.field.end())
+				if (currentPosition.first != field.field.end()){
 					currentPosition.first = ++currentPosition.first;//идем вверх
+				}
 			}
 		}
 	}
@@ -111,13 +98,16 @@ public:
 		if (currentPosition.first != prevPosition.first){
 			if (currentPosition.first > prevPosition.first){
 				prevPosition = currentPosition;
-				if (currentPosition.second != (*(field.field.begin() + dist_row)).begin())
-					currentPosition.second = --currentPosition.second;
+				if (currentPosition.second != ((*currentPosition.first).begin()) )
+					currentPosition.second = ++currentPosition.second;
+
+				/*if (currentPosition.second != (*field.field.begin()).end())
+					currentPosition.second = ++currentPosition.second;*/
 			}
 			else {
 				prevPosition = currentPosition;
-				if (currentPosition.second != (*(field.field.begin() + dist_row)).end())
-					currentPosition.second = ++currentPosition.second;
+				if (currentPosition.second != (*field.field.begin()).begin())
+					currentPosition.second = --currentPosition.second;
 			}
 		}
 		else {
@@ -134,7 +124,6 @@ public:
 		}
 	}
 	void goForward(Field &field){
-		ptrdiff_t dist_row = distance(vector<vector<Cell>>::iterator(currentPosition.first), vector<vector<Cell>>::iterator(field.field.end()));
 
 		if (currentPosition.first != prevPosition.first){//если индексы рядов не равны значит мы движемсья прямо между рядами
 			if (currentPosition.first > prevPosition.first){//определяем куда именно вниз/вверх
@@ -152,13 +141,13 @@ public:
 		else {//значит движемься между колонками в переделах одного ряда
 			if (currentPosition.second > prevPosition.second){//движемься слева направо
 				prevPosition = currentPosition;
-				if (currentPosition.second != (*(field.field.begin() + dist_row)).end())
-					currentPosition.second = ++currentPosition.second;
+				if (currentPosition.second != (*field.field.begin()).begin())
+					currentPosition.second = --currentPosition.second;
 			}
 			else {
 				prevPosition = currentPosition;
-				if (currentPosition.second != (*(field.field.begin() + dist_row)).begin())
-					currentPosition.second = --currentPosition.second;
+				if (currentPosition.second != (*field.field.begin()).end())
+					currentPosition.second = ++currentPosition.second;
 			}
 		}
 	}
@@ -170,8 +159,11 @@ private:
 int main(){
 	srand(time(NULL));
 	Cell copy;
+
 	Field F(pair<int, int>(5, 5), copy);//создание поля 5*5
 	Player P(pair<vector<vector<Cell>>::iterator, vector<Cell>::iterator>(F.beg));//создание и инициализация позиции игрока 0*0
+	P.switcher(1, F);
+	P.switcher(2, F);
 	P.switcher(1, F);
 
 
