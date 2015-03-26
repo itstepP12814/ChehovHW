f0155b559edc07893267955786f2f2cc7eedb03a
Pavel Chehov <good-1991@mail.ru>
2015-03-26 17:34:17 +0300

    продолжение

--- WinApi/HW/HW40/TicTacToe/Source.cpp
+++ WinApi/HW/HW40/TicTacToe/Source.cpp
@@ -3,8 +3,14 @@
 #include <tchar.h>
 #include <vector>
 #include "resource.h"
+using namespace std;
 
+struct CELL{
+	HWND hWnd;
+	int stat;
+};
 BOOL CALLBACK DlgProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+vector <CELL> CellVector;
 HBITMAP hbmX, hbmO;
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpszCmdLine, int nCmdShow)
 {
@@ -18,23 +24,49 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam){
 	case WM_INITDIALOG:{
 		HMENU MainMenu = LoadMenu(GetModuleHandle(0), MAKEINTRESOURCE(IDR_MENU1));
 		SetMenu(hWnd, MainMenu);
-		hbmX = LoadBitmap(GetModuleHandle(0), MAKEINTRESOURCE(IDB_BITMAP1));
-		hbmO = LoadBitmap(GetModuleHandle(0), MAKEINTRESOURCE(IDB_BITMAP2));
+		HWND hButton;
+		hbmX = LoadBitmap(GetModuleHandle(0), MAKEINTRESOURCE(IDB_BITMAP2));
+		hbmO = LoadBitmap(GetModuleHandle(0), MAKEINTRESOURCE(IDB_BITMAP1));
+		for (int i = 0; i < 9; ++i){//содержим в массиве состояние кнопок
+			hButton = GetDlgItem(hWnd, (IDC_BUTTON1 + i));
+			CellVector.push_back({hButton, 0});
+		}
 	}	return TRUE;
 	case WM_COMMAND: {
+		LRESULT status = SendMessage(GetDlgItem(hWnd, IDC_CHECK_FIRST_STEP), BM_GETCHECK, 0, 0);
+		for (int i = 0; i < 9; ++i){
+			if (LOWORD(wParam) == (IDC_BUTTON1 + i)){
+				HWND hButton = GetDlgItem(hWnd, (IDC_BUTTON1 + i));
+				SendMessage(hButton, BM_SETIMAGE, WPARAM(IMAGE_BITMAP), LPARAM(hbmX));
+				EnableWindow(hButton, 0);
+
+				for (vector<CELL>::iterator i = CellVector.begin(); i != CellVector.end(); ++i){
+					if (i->hWnd == hButton){
+						i->stat = 1;
+						break;
+					}
+				}
+				for (auto x: CellVector){
+					if (x.stat == 0){
+						SendMessage(x.hWnd, BM_SETIMAGE, WPARAM(IMAGE_BITMAP), LPARAM(hbmO));
+						EnableWindow(x.hWnd, 0);
+						x.stat = 1;
+						break;
+					}
+				}
+				break;
+			}
+		}
 		switch (wParam){
-		case (ID_EXIT) : {
+		case ID_EXIT: {
 			int res = MessageBox(0, TEXT("Вы уверены?"), TEXT("Выход?"), MB_YESNO | MB_ICONINFORMATION);
 			if (res == IDYES) EndDialog(hWnd, 0);
-			break;
 		}break;
-		case IDC_BUTTON1_1:{
-			HWND hButton = GetDlgItem(hWnd, IDC_BUTTON1_1);
-			SendMessage(hButton, STM_SETIMAGE, WPARAM(IMAGE_BITMAP), LPARAM(hbmX));
-		} break;
+		case IDC_CHECK_FIRST_STEP:{
+			//int res = MessageBox(0, TEXT("Вы уверены?"), TEXT("Выход?"), MB_YESNO | MB_ICONINFORMATION);
+		}break;
 		}
 	} return TRUE;
-
 	default: return FALSE;
 	}
 }
