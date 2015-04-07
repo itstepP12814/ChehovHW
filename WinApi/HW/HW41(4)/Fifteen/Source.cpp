12641ee0d6cbeb6e383a6c52b72efcb1d851517f
Pavel Chehov <good-1991@mail.ru>
2015-04-07 19:29:19 +0300

    update 15

--- WinApi/HW/HW41(4)/Fifteen/Source.cpp
+++ WinApi/HW/HW41(4)/Fifteen/Source.cpp
@@ -16,7 +16,7 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 
 class Model{
 public:
-	Model(size_t height_, size_t width_) : height(height_), width(width_), last(height*width), field(height, vector<int>(width, 0)){
+	Model(size_t height_, size_t width_) : height(height_), width(width_), last(16), field(height, vector<int>(width, 0)){
 		int numeros = 1;
 		for (int i = 0; i < height; ++i){
 			for (int j = 0; j < width; ++j){
@@ -74,20 +74,25 @@ struct coo{
 };
 
 map<HWND, coo> buttons;
-map<HWND, coo> ::iterator it;
+map<HWND, coo>::iterator it;
 
 BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	switch (message){
+
+	case WM_CLOSE:{
+		EndDialog(hWnd, 0); // закрываем модальный диалог
+	}return TRUE;
+
 	case WM_INITDIALOG:{
 		game = new Model(4, 4);
 		//SetWindowPos(hWnd, HWND_BOTTOM, 0, 0, 200 /*width*/, 200 /*height*/, SWP_NOMOVE);
 		HMENU MainMenu = LoadMenu(GetModuleHandle(0), MAKEINTRESOURCE(IDR_MENU1));
 		SetMenu(hWnd, MainMenu);
 		//создаём статик с помощью CreateWindowEx
-		for (int i = 0; i < game->height; ++i)
+		for (size_t i = 0; i < game->height; ++i)
 		{
-			for (int j = 0; j < game->width; ++j)
+			for (size_t j = 0; j < game->width; ++j)
 			{
 				HWND hButton = CreateWindowEx(0, TEXT("BUTTON"), 0,
 					WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
@@ -105,38 +110,39 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 
 	} return TRUE;
 
-	case WM_CLOSE:{
-		EndDialog(hWnd, 0); // закрываем модальный диалог
-	}return TRUE;
-
 	case WM_COMMAND:{
-	case ID_EXIT:{
-		int res = MessageBox(0, TEXT("Вы уверены?"), TEXT("Выход?"), MB_YESNO | MB_ICONINFORMATION);
-		if (res == IDYES) EndDialog(hWnd, 0);
-	} break;
-	default: {
-		it = buttons.find((HWND)lParam);
-		if (it != buttons.end()){
-			game->move(it->second.row, it->second.col);
-			/*if (game->win()){
-				int res = MessageBox(0, TEXT("Победа"), TEXT("Вы победили"), MB_OK | MB_ICONINFORMATION);
-				}*/
-			for (it = buttons.begin(); it != buttons.end(); ++it){//перерисовываем поле
-				int i = it->second.row; int j = it->second.col;
-				wsprintf(szText, TEXT("%d"), game->field[i][j]);
+		switch (wParam){
+		case ID_EXIT:{
+			int res = MessageBox(0, TEXT("Вы уверены?"), TEXT("Выход?"), MB_YESNO | MB_ICONINFORMATION);
+			if (res == IDYES) EndDialog(hWnd, 0);
+		} break;
+		case ID_NEWGAME:{
+			delete game;
+			game = new Model(4, 4);
+		} break;
+		default: {
+			it = buttons.find((HWND)lParam);
+			if (it != buttons.end()){
+				game->move(it->second.row, it->second.col);
+				for (it = buttons.begin(); it != buttons.end(); ++it){//перерисовываем поле
+					int i = it->second.row; int j = it->second.col;
+					wsprintf(szText, TEXT("%d"), game->field[i][j]);
 
-				if (game->field[i][j] == game->last) {
-					SetWindowText(it->first, NULL);
-					EnableWindow(it->first, 0);
+					if (game->field[i][j] == game->last) {
+						SetWindowText(it->first, NULL);
+						EnableWindow(it->first, 0);
+					}
+					else{
+						SetWindowText(it->first, szText);
+						EnableWindow(it->first, 1);
+					}
 				}
-				else{
-					SetWindowText(it->first, szText);
-					EnableWindow(it->first, 1);
+				if (game->win()){
+					int res = MessageBox(0, TEXT("Победа"), TEXT("Вы победили"), MB_OK | MB_ICONINFORMATION);
 				}
 			}
+		} break;
 		}
-	}break;
-	}return TRUE;
-
-	}return FALSE;
+	} return TRUE;
+	} return FALSE;
 }
