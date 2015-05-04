0f31dabec1fd7215912b2b2c050dc368f027117e
Pavel Chehov <good-1991@mail.ru>
2015-05-04 13:14:02 +0300

    исправлены неочеты с регулярками

--- C#/HW1/Digits.cs
+++ C#/HW1/Digits.cs
@@ -17,22 +17,32 @@ namespace ConsoleApplication
             {
                 Console.WriteLine("Введите первое число");
                 firstStr = Console.ReadLine();
-                if (!Regex.IsMatch(firstStr, "[0-9]")) return;
                 Console.WriteLine("Введите второе число");
                 secondStr = Console.ReadLine();
-                if (!Regex.IsMatch(secondStr, "[0-9]")) return;
-                first = Convert.ToInt32(firstStr);
-                second = Convert.ToInt32(secondStr);
-
-                for (int i = first; i <= second; ++i)
-                {
-                    for (int j = 0; j < i; ++j)
-                    {
-                        Console.Write(i + " ");
-                    }
-                    Console.WriteLine();
-                }
+                if (CompareString(firstStr) && CompareString(secondStr)) break;
+                else  Console.WriteLine("Повторите ввод");
             }
+            first = Convert.ToInt32(firstStr);
+            second = Convert.ToInt32(secondStr);
+            for (int i = first; i <= second; ++i)
+            {
+                for (int j = 0; j < i; ++j)
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        static bool CompareString(string str)
+        {
+            if (str == "") return false;
+            for (int i = 0; i < str.Length; ++i)
+            {
+                int temp = Convert.ToInt32(str[i]);
+                if (temp >=48 && temp <= 57) continue;
+                else return false;
+            }
+            return true;
         }
     }
 }
