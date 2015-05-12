6579062266e76d6f0f8133214c108b6d8b3e7813
Pavel Chehov <good-1991@mail.ru>
2015-05-12 16:41:23 +0300

    изменена структура папок

---
+++ C#/ClassWorks/MainClassLib/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainClassLib
+{
+    public class Class1
+    {
+        int n;
+        double v;
+        public int GetN()
+        {
+            return n;
+        }
+    }
+}
