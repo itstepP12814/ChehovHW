af9ec5d6acd182a4249cb3ae4f6d723740c84ea0
Pavel Chehov <good-1991@mail.ru>
2015-05-18 22:45:36 +0300

    добавлен WorldOfTanks

---
+++ C#/HomeWorks/HW3/WOT/Day7(Tanks)/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyClassLib.WorldOfTanks;
+
+namespace Day7_Tanks_
+{
+    class Program
+    {
+        static int FindByModel(string model_, Tank[] array)
+        {
+            int count = 0;
+            for (int i = 0; i < array.Length; ++i)
+            {
+                if (array[i].Model == model_) ++count;
+            }
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            Random ran = new Random();
+            Tank[] T34array = new Tank[5];
+            Tank[] panterArray = new Tank[5];
+            Tank[] winnersArray = new Tank[5];
+
+            for (int i = 0; i < 5; ++i) T34array[i] = new Tank(ran.Next(0, 100), ran.Next(0, 100), ran.Next(0, 100), "T34");
+            for (int i = 0; i < 5; ++i) panterArray[i] = new Tank(ran.Next(0, 100), ran.Next(0, 100), ran.Next(0, 100), "Panter");
+            for (int i = 0; i < 5; ++i)
+            {
+                Console.WriteLine(T34array[i].ToString() + "\n" + panterArray[i].ToString());
+                Console.WriteLine();
+                winnersArray[i] = T34array[i] ^ panterArray[i];
+                Console.WriteLine("Победил " + winnersArray[i].ToString());
+                Console.WriteLine();
+            }
+            if (FindByModel("Panter", winnersArray) >= 3) Console.WriteLine("Победила команда Пантер");
+            else Console.WriteLine("Победила команда Т34");
+        }
+    }
+}
---
+++ C#/HomeWorks/HW3/WOT/MyClassLib/WorldOfTanks/Tank.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLib.WorldOfTanks
+{
+    public class Tank
+    {//требуется объявить класс со спецификатором доступа public, чтобы он был доступен в сборке
+
+        public Tank(int ammo_, int armor_, int mobility_, string model_)
+        {
+            try
+            {
+                if (ammo_ > 100 || ammo_ < 0) throw new Exception("Неадекватное число процентов боекомплекта");
+                if (armor_ > 100 || armor_ < 0) throw new Exception("Неадекватное число процентов брони");
+                if (mobility_ > 100 || mobility_ < 0) throw new Exception("Неадекватное число процентов маневренности");
+                ammo = ammo_;
+                armor = armor_;
+                mobility = mobility_;
+                model = model_;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+        public override string ToString()
+        {
+            return String.Format("{0,6} Боекомплект: {1,2} Броня: {2,2} Маневренность: {3,2} ", model, ammo, armor, mobility);
+        }
+        public static Tank operator ^(Tank t1, Tank t2)
+        {
+            try
+            {
+                if (t1 == null || t2 == null) throw new ArgumentNullException("Передан пустой аргумент для сравнения");
+                if (t1.ammo > t2.ammo)
+                {
+                    if (t1.armor > t2.armor) return t1;
+                    else
+                    {
+                        if (t1.mobility > t2.mobility) return t1;
+                        else return t2;
+                    }
+                }
+                else
+                {
+                    if (t1.armor > t2.armor)
+                    {
+                        if (t1.mobility > t2.mobility) return t1;
+                        else return t2;
+                    }
+                    else return t2;
+                }
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+        public string Model
+        {
+            get { return model; }
+        }
+        private int ammo;
+        private int armor;
+        private int mobility;
+        private string model;
+
+    }
+
+}
